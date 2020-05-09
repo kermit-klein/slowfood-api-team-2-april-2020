3ea1fa519b1649c7ca76bdfe5b6571fdfd9aca75
allezz <aerbay@gmail.com>
2020-05-09 15:48:05 +0200

    Edit order controller file, add serializer json method

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -4,13 +4,20 @@ class Api::V1::OrdersController < ApplicationController
   def create
     order = Order.create(user_id: params[:user])
     order.order_items.create(menu_item_id: params[:menu_item])
-    render json: { id: order.id, message: 'Item added to order' }
+    render json: create_json_response(order)
   end
 
   def update
     order = Order.find(params[:id])
     menu_item = MenuItem.find(params[:menu_item])
     order.order_items.create(menu_item: menu_item)
-    render json: { message: 'Item added to order', id: order.id }
+    render json: create_json_response(order)
+  end
+
+  private
+
+  def create_json_response(order)
+    json = { order: OrderSerializer.new(order) }
+    json.merge!(message: 'The product has been added to your order')
   end
 end
